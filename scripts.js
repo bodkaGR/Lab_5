d7405c38413076ca3d69da3fe1bf18d1760c5209
bodkaGR <grontkovskijboda@gmail.com>
2023-12-05 23:44:56 +0100

    done 2 task

---
+++ scripts.js
@@ -0,0 +1,31 @@
+//task 1
+function chang_text(){
+    var header = document.getElementById('header');
+    var footer = document.getElementById('footer');
+
+    var headerText = header.innerText;
+    var footerText = footer.innerText;
+
+    header.innerText = footerText;
+    footer.innerText = headerText;
+}
+
+//task 2
+function diamond_square(){
+    const input_value_1 = parseFloat(document.getElementById('input_1').value);
+    const input_value_2 = parseFloat(document.getElementById('input_2').value);
+
+    console.log('d1: ' + input_value_1);
+    console.log('d2: ' + input_value_2);
+
+    const square_output = document.getElementById('squareOutput')
+
+    if (isNaN(input_value_1) || isNaN(input_value_2)){
+        alert('Error: d1 and d2 can not be null');
+        square_output.textContent = 'error';
+    }else{
+        const result = 0.5*input_value_1*input_value_2;
+        square_output.textContent = 'Square: ' + result;
+        console.log('square: ' + result);
+    }
+}
